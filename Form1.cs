985036ba7e9ceee49117608e150b725e41518f81
fotuka <antda08@yandex.ru>
2022-12-29 12:51:08 +0300

    Transfer JsonToBinary class from BinReader to buil-in dll

--- Form1.cs
+++ Form1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
-using BinReader;
+using JsonBinLib;
 using Newtonsoft.Json;
 
 namespace BinaryToJSONConverterApp
