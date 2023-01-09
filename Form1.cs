5add2d2e937e3cb8933034e70264fda985b88f91
fotuka <antda08@yandex.ru>
2023-01-09 15:04:11 +0300

    Reformat code

--- Form1.cs
+++ Form1.cs
@@ -7,10 +7,10 @@ using JsonBinLib;
 using Newtonsoft.Json;
 
 namespace BinaryToJSONConverterApp
-{   
+{
     public partial class Form1 : Form
     {
-        public string openedJson = "";
+        public string openedJson;
         public string savedBinary = "";
 
         public Form1()
@@ -24,9 +24,6 @@ namespace BinaryToJSONConverterApp
             {
                 InitialDirectory = @"D:\",
                 Title = "Browse JSON Files",
-                CheckFileExists = true,
-                CheckPathExists = true,
-                FilterIndex = 1,
                 RestoreDirectory = true,
                 ReadOnlyChecked = true,
                 ShowReadOnly = true
@@ -41,12 +38,11 @@ namespace BinaryToJSONConverterApp
 
         internal void buttonSaveBrowse_Click(object sender, EventArgs e)
         {
-            SaveFileDialog save_file = new SaveFileDialog();                       
+            SaveFileDialog save_file = new SaveFileDialog();
             save_file.Filter = "Baikal7|*.00|Baikal8|*.xx|Sigma|*.bin";
 
-            save_file.FilterIndex = 1;
             if (save_file.ShowDialog() == DialogResult.OK)
-            {                
+            {
                 textBoxSaveFolder.Text = save_file.FileName;
                 savedBinary = save_file.FileName;
             }
@@ -54,45 +50,53 @@ namespace BinaryToJSONConverterApp
 
         internal void buttonGo_Click(object sender, EventArgs e)
         {
-            JsonClass seisFile = new JsonClass();
-
-            using (StreamReader r = new StreamReader(openedJson))
-            {
-                string jsonInput = r.ReadToEnd();
-                seisFile = JsonConvert.DeserializeObject<JsonClass>(jsonInput);
-            }
-
-            float[] signal = seisFile.signal;
-            Int32[] signalInInt32 = new Int32[signal.Length];
-
-            double maximumOfSignal = signal.Max();
-            double minimumOfSignal = signal.Min();
-            double maximumOfAmplitude = Math.Max(Math.Abs(maximumOfSignal), Math.Abs(minimumOfSignal));
-
-            double coefNorm = Convert.ToDouble(1024) / maximumOfAmplitude;
-            for (int i = 0; i < signal.Length; i++)
-            {
-                signalInInt32[i] = Convert.ToInt32(signal[i] * coefNorm);
-            }
-
-            double latitude = Convert.ToDouble(seisFile.N_wgs84_latitude.PadRight(8, '0').Substring(0,8).Replace('.',','));
-            double longitude = Convert.ToDouble(seisFile.E_wgs84_longitude.PadRight(8, '0').Substring(0, 8).Replace('.', ','));
-
-            DateTime dateTimeStart = seisFile.start_time;
-
-            
+            JsonClass seisFile = ReadClassFromJson(openedJson);
+                        
+            Int32[] signalInInt32 = NormalizeNConvertSignal(seisFile.signal);            
+            double latitude = Convert.ToDouble(seisFile.NWGS84Latitude.PadRight(8, '0').Substring(0, 8).Replace('.', ','));
+            double longitude = Convert.ToDouble(seisFile.EWGS84Longitude.PadRight(8, '0').Substring(0, 8).Replace('.', ','));
+            DateTime dateTimeStart = seisFile.startTime;
 
             JsonFileBinary jsonya = new JsonFileBinary(savedBinary, signalInInt32, latitude, longitude, dateTimeStart);
             jsonya.WriteBinaryFile(savedBinary);
-            MessageBox.Show("Гатова");
+
+            MessageBox.Show("Готова");
         }
-    }
+
+        public Int32[] NormalizeNConvertSignal(float[] originSignal)
+        {
+            Int32[] signalInInt32 = new Int32[originSignal.Length];
+            double maximumOfSignal = originSignal.Max();
+            double minimumOfSignal = originSignal.Min();
+            double maximumOfAmplitude = Math.Max(Math.Abs(maximumOfSignal), Math.Abs(minimumOfSignal));
+            double coefNorm = Convert.ToDouble(1024) / maximumOfAmplitude;
+            
+            for (int i = 0; i < originSignal.Length; i++)
+            {
+                signalInInt32[i] = Convert.ToInt32(originSignal[i] * coefNorm);
+            }
+
+            return signalInInt32;
+        }
+
+        public JsonClass ReadClassFromJson(string filePath)
+        {
+            JsonClass seisFile = new JsonClass();
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string jsonInput = reader.ReadToEnd();
+                seisFile = JsonConvert.DeserializeObject<JsonClass>(jsonInput);
+            }
+
+            return seisFile;
+        }
+    }    
     public class JsonClass
     {
-        public DateTime start_time { get; set; }
-        public string N_wgs84_latitude { get; set; }
-        public string E_wgs84_longitude { get; set; }
-        public string filename { get; set; }
+        public DateTime startTime { get; set; }
+        public string NWGS84Latitude { get; set; }
+        public string EWGS84Longitude { get; set; }
+        public string fileName { get; set; }
         public float[] signal { get; set; }
     }
 }
