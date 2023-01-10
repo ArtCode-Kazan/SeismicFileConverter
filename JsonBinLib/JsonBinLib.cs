88cdc2c33249cf37065a9e35ff497a1399e0224b
fotuka <antda08@yandex.ru>
2023-01-10 10:24:01 +0300

    Delete other extension from lib

--- JsonBinLib/JsonBinLib.cs
+++ JsonBinLib/JsonBinLib.cs
@@ -41,7 +41,7 @@ namespace JsonBinLib
         }
 
         public JsonFileBinary(
-            string pathToJson, 
+            string pathToJson,
             string pathToSave
             )
         {
@@ -55,76 +55,28 @@ namespace JsonBinLib
             this.dateTimeStart = jsonya.start_time;
 
         }
-        
+
         public void WriteBinaryFile(string path)
         {
             using (FileStream filestream = new FileStream(path, FileMode.Create))
             {
                 using (BinaryWriter binaryWriter = new BinaryWriter(filestream))
                 {
+                    binaryWriter.Seek(0, SeekOrigin.Begin);
+                    binaryWriter.Write(BitConverter.GetBytes(Convert.ToUInt16(this.channelCount)));
+                    binaryWriter.Seek(22, SeekOrigin.Begin);
+                    binaryWriter.Write(BitConverter.GetBytes(Convert.ToUInt16(this.frequency)));
+                    binaryWriter.Seek(80, SeekOrigin.Begin);
+                    binaryWriter.Write(BitConverter.GetBytes(Convert.ToDouble(this.longitude.PadRight(8, '0').Substring(0, 8).Replace('.', ','))));
+                    binaryWriter.Seek(72, SeekOrigin.Begin);
+                    binaryWriter.Write(BitConverter.GetBytes(Convert.ToDouble(this.latitude.PadRight(8, '0').Substring(0, 8).Replace('.', ','))));
+                    binaryWriter.Seek(104, SeekOrigin.Begin);
 
-                    if (FileExtension == SigmaExt)
-                    {
-                        binaryWriter.Seek(12, SeekOrigin.Begin);
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToUInt16(this.channelCount)));
-                        binaryWriter.Seek(24, SeekOrigin.Begin);
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToUInt16(this.frequency)));
-                        binaryWriter.Seek(40, SeekOrigin.Begin);                        
-                        binaryWriter.Write(Convert.ToString(this.latitude.PadRight(8, '0').Substring(0, 8).Replace('.', ','))); //string 8 digits
-                        binaryWriter.Seek(48, SeekOrigin.Begin);                        
-                        binaryWriter.Write(Convert.ToString(this.latitude.PadRight(8, '0').Substring(0, 8).Replace('.', ','))); //string 8 digits
-                        binaryWriter.Seek(60, SeekOrigin.Begin);
-                        string date = Convert.ToString(this.dateTimeStart.Year).Substring(2, 2)
-                            + Convert.ToString(this.dateTimeStart.Month).PadLeft(2, '0')
-                        + Convert.ToString(this.dateTimeStart.Day).PadLeft(2, '0');
-                        string time = Convert.ToString(this.dateTimeStart.Hour).PadLeft(2, '0')
-                            + Convert.ToString(this.dateTimeStart.Minute).PadLeft(2, '0')
-                        + Convert.ToString(this.dateTimeStart.Second).PadLeft(2, '0');
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToUInt32(date)));
-                        binaryWriter.Seek(64, SeekOrigin.Begin);
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToUInt32(time)));
-                    }
+                    DateTime constDatetime = new DateTime(1980, 1, 1);
+                    double secondsDuraion = (this.dateTimeStart - constDatetime).TotalSeconds;
+                    ulong secondsForWriting = Convert.ToUInt64(secondsDuraion) * 256000000;
 
-                    else if (FileExtension == Baikal7Ext) // main
-                    {
-                        binaryWriter.Seek(0, SeekOrigin.Begin);
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToUInt16(this.channelCount)));
-                        binaryWriter.Seek(22, SeekOrigin.Begin);
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToUInt16(this.frequency)));
-                        binaryWriter.Seek(80, SeekOrigin.Begin);
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToDouble(this.longitude.PadRight(8, '0').Substring(0, 8).Replace('.', ','))));
-                        binaryWriter.Seek(72, SeekOrigin.Begin);
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToDouble(this.latitude.PadRight(8, '0').Substring(0, 8).Replace('.', ','))));
-                        binaryWriter.Seek(104, SeekOrigin.Begin);
-
-                        DateTime constDatetime = new DateTime(1980, 1, 1);
-                        double secondsDuraion = (this.dateTimeStart - constDatetime).TotalSeconds;
-                        ulong secondsForWriting = Convert.ToUInt64(secondsDuraion) * 256000000;
-
-                        binaryWriter.Write(BitConverter.GetBytes(secondsForWriting));
-                    }
-
-                    else if (FileExtension == Baikal8Ext)
-                    {
-                        binaryWriter.Seek(0, SeekOrigin.Begin);
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToUInt16(this.channelCount)));
-                        binaryWriter.Seek(6, SeekOrigin.Begin);                        
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToUInt16(this.dateTimeStart.Day)));
-                        binaryWriter.Seek(8, SeekOrigin.Begin);                        
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToUInt16(this.dateTimeStart.Month)));
-                        binaryWriter.Seek(10, SeekOrigin.Begin);                        
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToUInt16(this.dateTimeStart.Year)));
-                        binaryWriter.Seek(48, SeekOrigin.Begin);
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToDouble(1) / this.frequency));
-                        binaryWriter.Seek(56, SeekOrigin.Begin);                        
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToDouble(this.dateTimeStart.Second)));
-                        binaryWriter.Seek(72, SeekOrigin.Begin);
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToDouble(this.latitude.PadRight(8, '0').Substring(0, 8).Replace('.', ','))));
-                        binaryWriter.Seek(80, SeekOrigin.Begin);
-                        binaryWriter.Write(BitConverter.GetBytes(Convert.ToDouble(this.longitude.PadRight(8, '0').Substring(0, 8).Replace('.', ','))));
-                    }
-
-                    
+                    binaryWriter.Write(BitConverter.GetBytes(secondsForWriting));
 
                     int headerMemorySize = 120 + 72 * this.channelCount;
                     int columnIndex = 1 * sizeof(int);
@@ -139,13 +91,7 @@ namespace JsonBinLib
                 }
             }
         }
-        public string FileExtension
-        {   
-            get
-            {
-                return Path.GetExtension(this.path).Substring(1);
-            }
-        }
+
         public JsonClass returnJsonClass(string pathToJson)
         {
             JsonClass seisFile;
