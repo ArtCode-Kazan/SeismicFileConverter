2e02a8dfe244948af25879ef3d5b98ba72991e16
fotuka <79892964+fotuka@users.noreply.github.com>
2023-01-12 01:09:29 +0300

    Pull request 1 edits done

--- JsonBinLib/JsonBinLib.cs
+++ JsonBinLib/JsonBinLib.cs
@@ -7,82 +7,69 @@ namespace JsonBinLib
 {
     public class SeisBinaryFile
     {
-        public const string componentsOrder = "ZXY";
+        public const string ComponentsOrder = "ZXY";
+        private const int NormalizationMaximum = 32768;
         private int componentOffset = 1;
-        private string savePath;
-        private float[] signal;
-        private string latitude;
-        private string longitude;
-        private DateTime dateTimeStart;        
-        private int frequency;
+                
+        public JsonDataContainer jsonInfo;
+        public string savePath;
 
-        public SeisBinaryFile(
-            string pathToSaveFile,
-            float[] signal,
-            string latitude = "",
-            string longitude = "",
-            DateTime dateTimeStart = new DateTime(),
-            int frequency = 1000
-            )
+        public SeisBinaryFile(JsonDataContainer jsonDeserialized, string savePath)
         {
-            this.savePath = pathToSaveFile;
-            this.signal = signal;            
-            this.frequency = frequency;
-            this.latitude = latitude;
-            this.longitude = longitude;
-            this.dateTimeStart = dateTimeStart;
+            this.jsonInfo = jsonDeserialized;
+            this.savePath = savePath;
         }
-        public int channelsCount
+        public UInt16 channelsCount
         { 
             get 
             { 
-                return componentsOrder.Length; 
+                return Convert.ToUInt16(ComponentsOrder.Length); 
             } 
         }
-        public Int32[] NormalizeNConvertSignal(float[] originSignal)
+        public Int32[] NormalizeSignal(float[] originSignal)
         {
-            Int32[] signalInInt32 = new Int32[originSignal.Length];
+            Int32[] normalizedSignal = new Int32[originSignal.Length];
             double maximumOfSignal = originSignal.Max();
             double minimumOfSignal = originSignal.Min();
             double maximumOfAmplitude = Math.Max(Math.Abs(maximumOfSignal), Math.Abs(minimumOfSignal));
-            double coeffNorm = Convert.ToDouble(1024) / maximumOfAmplitude;
+            double coeffNorm = Convert.ToDouble(NormalizationMaximum) / maximumOfAmplitude;
 
             for (int i = 0; i < originSignal.Length; i++)
             {
-                signalInInt32[i] = Convert.ToInt32(originSignal[i] * coeffNorm);
+                normalizedSignal[i] = Convert.ToInt32(originSignal[i] * coeffNorm);
             }
 
-            return signalInInt32;
+            return normalizedSignal;
         }
-        public void Save()
+        public void SaveToBaykal7Format()
         {                        
-            Int32[] normalSignal = NormalizeNConvertSignal(this.signal);                                                
+            Int32[] normalSignal = NormalizeSignal(this.jsonInfo.signal);                                                
 
             using (FileStream filestream = new FileStream(this.savePath, FileMode.Create))
             {
                 using (BinaryWriter binaryWriter = new BinaryWriter(filestream))
                 {
                     binaryWriter.Seek(0, SeekOrigin.Begin);
-                    binaryWriter.Write(BitConverter.GetBytes(Convert.ToUInt16(channelsCount)));
+                    binaryWriter.Write(BitConverter.GetBytes(channelsCount));
                     binaryWriter.Seek(22, SeekOrigin.Begin);
-                    binaryWriter.Write(BitConverter.GetBytes(Convert.ToUInt16(this.frequency)));
+                    binaryWriter.Write(BitConverter.GetBytes(this.jsonInfo.frequency));
                     binaryWriter.Seek(80, SeekOrigin.Begin);
-                    binaryWriter.Write(BitConverter.GetBytes(Convert.ToDouble(this.longitude.PadRight(8, '0').Substring(0, 8).Replace('.', ','))));
+                    binaryWriter.Write(BitConverter.GetBytes(this.jsonInfo.E_wgs84_longitude));
                     binaryWriter.Seek(72, SeekOrigin.Begin);
-                    binaryWriter.Write(BitConverter.GetBytes(Convert.ToDouble(this.latitude.PadRight(8, '0').Substring(0, 8).Replace('.', ','))));
-                    
+                    binaryWriter.Write(BitConverter.GetBytes(this.jsonInfo.N_wgs84_latitude));
+
                     binaryWriter.Seek(104, SeekOrigin.Begin);
                     DateTime constDatetime = new DateTime(1980, 1, 1);
-                    double secondsDuraion = (this.dateTimeStart - constDatetime).TotalSeconds;
+                    double secondsDuraion = (this.jsonInfo.start_time - constDatetime).TotalSeconds;
                     ulong secondsForWriting = Convert.ToUInt64(secondsDuraion) * 256000000;
                     binaryWriter.Write(BitConverter.GetBytes(secondsForWriting));
 
                     int headerMemorySize = 120 + 72 * channelsCount;
-                    int columnIndex = 1 * sizeof(int);
+                    int columnIndex = this.componentOffset * sizeof(int);
                     int stridesSize = sizeof(int) * channelsCount;
-                    binaryWriter.Seek(headerMemorySize + columnIndex, 0);
+                    binaryWriter.Seek(headerMemorySize + columnIndex, SeekOrigin.Begin);
 
-                    for (int i = 0; i < signal.Length; i++)
+                    for (int i = 0; i < normalSignal.Length; i++)
                     {
                         binaryWriter.Write(BitConverter.GetBytes(normalSignal[i]));
                         binaryWriter.Seek(stridesSize - sizeof(int), SeekOrigin.Current);
@@ -91,29 +78,30 @@ namespace JsonBinLib
             }
         }                
     }
-    public class SeisFileConverter
+    public class SeisJsonParser
     {
         public string pathToJsonFile;
-        public JsonClass jsonInfo;
+        public JsonDataContainer jsonDeserialized;
 
-        public SeisFileConverter(string pathToJsonFile, string pathToSaveFolder)
+        public SeisJsonParser(string pathToJsonFile)
         {
-            this.pathToJsonFile = pathToJsonFile;            
-        }
-        private void ParseJson()
-        {         
+            this.pathToJsonFile = pathToJsonFile;
+
             using (StreamReader reader = new StreamReader(this.pathToJsonFile))
             {
-                this.jsonInfo = JsonConvert.DeserializeObject<JsonClass>(reader.ReadToEnd());
+                this.jsonDeserialized = JsonConvert.DeserializeObject<JsonDataContainer>(reader.ReadToEnd());
             }
         }
+        
     }
-    public class JsonClass
+    public class JsonDataContainer
     {
         public DateTime start_time { get; set; }
         public double N_wgs84_latitude { get; set; }
         public double E_wgs84_longitude { get; set; }
         public string filename { get; set; }
         public float[] signal { get; set; }
+        public int frequency { get; set; }
+        public int componentName { get; set; }
     }
 }
--- MainConverterWindow.cs
+++ MainConverterWindow.cs
@@ -8,8 +8,8 @@ namespace BinaryToJSONConverterApp
 {
     public partial class MainConverterWindow : Form
     {
-        public List<string> pathsToJsonFiles = new List<string>();
-        public string pathToSaveBinaryFileFolder = "";
+        public List<string> pathsToJson = new List<string>();
+        public string pathSaveBinaryFolder = "";
         public string helpFileName = "JsonСonverter.chm";
 
         public MainConverterWindow()
@@ -25,7 +25,7 @@ namespace BinaryToJSONConverterApp
                 textBoxLoadFromFolder.Text = "";
                 foreach (String path in openFileDialog.FileNames)
                 {
-                    this.pathsToJsonFiles.Add(path);
+                    this.pathsToJson.Add(path);
                     textBoxLoadFromFolder.Text += openFileDialog.FileName + ";";
                 }                
             }
@@ -36,21 +36,22 @@ namespace BinaryToJSONConverterApp
             if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
             {
                 textBoxSaveFolder.Text = folderBrowserDialog.SelectedPath;
-                this.pathToSaveBinaryFileFolder = folderBrowserDialog.SelectedPath;
+                this.pathSaveBinaryFolder = folderBrowserDialog.SelectedPath;
             }
         }
 
         internal void buttonConvert_Click(object sender, EventArgs e)
-        {
-            int i = 1;
-
-            foreach (string path in this.pathsToJsonFiles)            
+        {            
+            for (int i = 0; i < this.pathsToJson.Count; i++)  
             {
-                SeisFileConverter converter = new SeisFileConverter(path, this.pathToSaveBinaryFileFolder);
-                converter.SaveBinary();     
-                toolStripStatusLabel.Text = "Processing...(" + i + "/" + Convert.ToString(this.pathsToJsonFiles.Count) + ")";                
-                statusStrip.Refresh();
-                i++;
+                string path = this.pathsToJson[i];
+                SeisJsonParser jsonParser = new SeisJsonParser(path);                
+                string binaryFileName = jsonParser.jsonDeserialized.filename + ".00";
+                string pathSaveBinary = Path.Combine(this.pathSaveBinaryFolder, binaryFileName);                
+    
+                SeisBinaryFile binaryFile = new SeisBinaryFile(jsonParser.jsonDeserialized, path);
+                toolStripStatusLabel.Text = "Processing...(" + (i + 1) + "/" + Convert.ToString(this.pathsToJson.Count) + ")";                
+                statusStrip.Refresh();             
             }
 
             toolStripStatusLabel.Text = "Success";
@@ -64,7 +65,6 @@ namespace BinaryToJSONConverterApp
             {
                 Help.ShowHelp(this, exeDirectory + "\\" + helpFileName);
             }
-
             else
             {
                 MessageBox.Show("Help file not found.");
