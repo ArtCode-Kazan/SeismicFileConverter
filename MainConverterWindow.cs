ded5ca775733f1d6ac865ca25d54a3629eef6d5d
fotuka <antda08@yandex.ru>
2023-01-11 13:19:12 +0300

    Delete useless usings

--- MainConverterWindow.cs
+++ MainConverterWindow.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Windows.Forms;
 using JsonBinLib;
-using Newtonsoft.Json;
 
 namespace BinaryToJSONConverterApp
 {
