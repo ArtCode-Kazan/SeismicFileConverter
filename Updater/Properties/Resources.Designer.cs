ad588d5ddd37f999b918993e06a6c51aa3cdd147
fotuka <antda08@yandex.ru>
2023-01-19 17:12:53 +0300

    One little mistake edit

--- Updater/Properties/Resources.Designer.cs
+++ Updater/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SeisJsonConveterUpdater.Properties {
+namespace SeisJsonConverterUpdater.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SeisJsonConveterUpdater.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SeisJsonConveterUpdater.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SeisJsonConverterUpdater.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Updater/Properties/Settings.Designer.cs
+++ Updater/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SeisJsonConveterUpdater.Properties {
+namespace SeisJsonConverterUpdater.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
