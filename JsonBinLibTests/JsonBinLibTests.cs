2d222dc1f1533ed09e6e90fb9b44c26dee742243
fotuka <antda08@yandex.ru>
2023-01-27 09:40:19 +0300

    Main tests finish

--- JsonBinLibTests/JsonBinLibTests.cs
+++ JsonBinLibTests/JsonBinLibTests.cs
@@ -2,89 +2,53 @@
 using JsonBinLib;
 using System;
 using System.IO;
+using Moq;
 
 namespace JsonBinLibTests
 {
+    public class Func
+    {
+        public static DateTime AppendSecondsToDateTimeBaikal7(ulong secondsamount)
+        {
+            DateTime Baikal7 = new DateTime(1980, 1, 1, 0, 0, 0).AddSeconds(secondsamount);
+            return Baikal7;
+        }
+
+        public static int[] RandomArray(int length)
+        {
+            int[] arr = new int[length];
+            Random rand = new Random();
+            arr[0] = -32768;
+            arr[1] = 32768;
+
+            for (int i = 2; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(-32768, 32768);
+            }
+            return arr;
+        }
+
+        public static float[] FormatArray(int[] array, float coefficient = 35000)
+        {
+            float[] formatArr = new float[array.Length];
+
+            for (int i = 0; i < formatArr.Length; i++)
+            {
+                formatArr[i] = (float)array[i] / coefficient;
+            }
+            return formatArr;
+        }
+    }
     [TestClass]
     public class JsonBinLibTest
     {
         [TestMethod]
-        [DataRow(@"C:\Users\user\Desktop\jsontests\input.txt", @"C:\Users\user\Desktop\jsontests\output.txt")]
-        [DataRow(@"C:\Users\user\Desktop\jsontests\inputBig.txt", @"C:\Users\user\Desktop\jsontests\outputBig.txt")]
-        public void TestNormalizeSignal(string inPath, string outPath)
+        public void Baikal7HeaderMemorySize()
         {
-            float[] inputArray = new float[50];
-            Int32[] outputArray = new Int32[50];
-            Int32[] outputArrayOrigin = new Int32[50];
-
-            string[] lines = File.ReadAllLines(inPath);
-            int i = 0;
-
-            foreach (string s in lines)
-            {
-                inputArray[i] = float.Parse(s);
-                i++;
-            }
-
-            i = 0;
-            lines = File.ReadAllLines(outPath);
-
-            foreach (string s in lines)
-            {
-                outputArrayOrigin[i] = Int32.Parse(s);
-                i++;
-            }
-
-            SeisBinaryFile testclass = new SeisBinaryFile(new JsonDataContainer(), "");
-            outputArray = testclass.NormalizeSignal(inputArray);
-
-            for (int j = 0; j < outputArray.Length; j++)
-            {
-                Assert.AreEqual(outputArrayOrigin[j], outputArray[j]);
-            }
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(DivideByZeroException), "Invalid currency.(zero)")]
-        [DataRow(@"C:\Users\user\Desktop\jsontests\inputZero.txt", @"C:\Users\user\Desktop\jsontests\outputZero.txt")]
-        [DataRow(@"C:\Users\user\Desktop\jsontests\inputSingle.txt", @"C:\Users\user\Desktop\jsontests\outputSingle.txt")]
-        public void TestNormalizeSignalZeroDivision(string inPath, string outPath)
-        {
-            float[] inputArray = new float[50];
-            Int32[] outputArray = new Int32[50];
-            Int32[] outputArrayOrigin = new Int32[50];
-
-            string[] lines = File.ReadAllLines(inPath);
-            int i = 0;
-
-            foreach (string s in lines)
-            {
-                inputArray[i] = float.Parse(s);
-                i++;
-            }
-
-            i = 0;
-            lines = File.ReadAllLines(outPath);
-
-            foreach (string s in lines)
-            {
-                outputArrayOrigin[i] = Int32.Parse(s);
-                i++;
-            }
-
-            SeisBinaryFile testclass = new SeisBinaryFile(new JsonDataContainer(), "");
-            outputArray = testclass.NormalizeSignal(inputArray);
-
-            for (int j = 0; j < outputArray.Length; j++)
-            {
-                Assert.AreEqual(outputArrayOrigin[j], outputArray[j]);
-            }
-        }
-
-        public DateTime AppendSecondsToDateTimeBaikal7(ulong secondsamount)
-        { 
-            DateTime Baikal7 = new DateTime(1980, 1, 1, 0, 0, 0).AddSeconds(secondsamount);
-            return Baikal7;
+            var mock = new Mock<JsonDataContainer>() { CallBase = true };
+            SeisBinaryFile testclass = new SeisBinaryFile(mock.Object, "");
+            int actualSeconds = testclass.Baikal7HeaderMemorySize;
+            Assert.AreEqual(336, actualSeconds);
         }
 
         [TestMethod]
@@ -95,26 +59,25 @@ namespace JsonBinLibTests
         [DataRow((UInt64)11911, (UInt64)3049216000000)]
         public void TestGetBaikal7SecondsForWriting(ulong realSeconds, ulong formatSeconds)
         {
-            SeisBinaryFile testclass = new SeisBinaryFile(new JsonDataContainer(), "");
-            ulong actualSeconds = testclass.GetBaikal7SecondsForWriting(AppendSecondsToDateTimeBaikal7(realSeconds));
+            var mock = new Mock<JsonDataContainer>() { CallBase = true };
+            SeisBinaryFile testclass = new SeisBinaryFile(mock.Object, "");
+            ulong actualSeconds = testclass.GetBaikal7SecondsForWriting(Func.AppendSecondsToDateTimeBaikal7(realSeconds));
             Assert.AreEqual(formatSeconds, actualSeconds);
         }
 
         [TestMethod]
-        public void Baikal7HeaderMemorySize()
+        public void TestNormalizeSignal()
         {
-            SeisBinaryFile testclass = new SeisBinaryFile(new JsonDataContainer(), "");
-            int actualSeconds = testclass.Baikal7HeaderMemorySize;
-            Assert.AreEqual(336, actualSeconds);
-        }
-        
-        [TestMethod]
-        [DataRow((UInt64)10, (UInt64)2560000000)]
-        public void Baikal7HeaderMemorySize(int signalLenght, int byteLenght)
-        {
-            SeisBinaryFile testclass = new SeisBinaryFile(new JsonDataContainer(startTime), "");
-            int actualSeconds = testclass.Baikal7HeaderMemorySize;
-            Assert.AreEqual(336, actualSeconds);
+            int[] outputArrayOrigin = Func.RandomArray(1000);
+            float[] inputArray = Func.FormatArray(outputArrayOrigin);
+
+            var mock = new Mock<SeisBinaryFile>(new JsonDataContainer(), "") { CallBase = true };
+            int[] outputArrayProgram = mock.Object.NormalizeSignal(inputArray);
+
+            for (int j = 0; j < outputArrayProgram.Length; j++)
+            {
+                Assert.AreEqual(outputArrayOrigin[j], outputArrayProgram[j]);
+            }
         }
     }
 }
